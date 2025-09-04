7b1c1d4a2139140511bbf3204020db44657dc3dc
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-04 09:26:24 +0530

    Promises

--- promises.js
+++ promises.js
@@ -8,6 +8,23 @@ const promise = new Promise((resolve, reject) => {
 
 // promise.then(result => console.log(result))
 
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'HIII')
+})
+
+const promise3 = new Promise((resolve, reject) =>{
+  setTimeout(resolve, 1000, 'POOKIE')
+})
+
+const promise4 = new Promise((resolve, reject) =>{
+  setTimeout(resolve, 3000, 'Is it me you are looking for?')
+})           
+
+Promise.all([promise, promise2, promise3, promise4])
+  .then(values =>{
+    console.log(values);
+  })
+
 promise
   .then(result => result + '!')
   .then(result2 => result2 + '?')

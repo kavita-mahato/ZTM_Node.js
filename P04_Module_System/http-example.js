5bb4c29a2d2a0a64dcc2a14eba7391ddb244026c
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-05 11:39:28 +0530

    HTTP Request

---
+++ P04_Module_System/http-example.js
@@ -0,0 +1,19 @@
+// HTTP Request
+// HTTP Request is a way to send a request to a server and get a response.
+//.on() is a method that is used to listen for events.
+//.end() is a method that is used to end the request.
+//.request() is a method that is used to create a request.
+//.res.on() is a method that is used to listen for events on the response.
+
+const http = require('http'); 
+
+const request = http.request('http://google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`Data chunk: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log('No more data');
+    });
+});
+
+request.end();

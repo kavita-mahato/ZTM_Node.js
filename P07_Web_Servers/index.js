fbdd88b76187da7a5f30bb483aa9544cf30a3f32
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-06 15:20:51 +0530

    Parameterized URLs

--- P07_Web_Servers/index.js
+++ P07_Web_Servers/index.js
@@ -4,18 +4,37 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+  {
+    id: 0,
+    name: "Kavita Mahato"
+  },
+  {
+    id: 1,
+    name: "Antara Mahanty"
+  },
+  {
+    id: 2,
+    name: "Anirban Mahato"
+  }
+]
+
 server.on("request", (req, res) => { 
-  if(req.url === '/friends'){
+  const items = req.url.split('/');
+  // /friends/2 => ['', 'friends', '2']
+  if(items[1] === 'friends'){
     // res.writeHead(200, {
     //   "Content-Type": "application/json",
     // });
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-      id: 1,
-      name: "Kavita Mahato",
-    }));
-  }else if(req.url === '/messages'){
+    if(items.length === 3){
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    }else{
+      res.end(JSON.stringify(friends));
+    }
+  }else if(items[1] === 'messages'){
     res.setHeader('Content-Type', 'text/html'); 
     res.write('<html>');
     res.write('<body>');
@@ -25,9 +44,6 @@ server.on("request", (req, res) => {
     res.write('</ul>');
     res.write('</body>');
     res.write('</html>');
-    res.end();
-  }else{
-    res.statusCode = 404;
     res.end();
   }
 });

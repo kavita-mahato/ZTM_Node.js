35bc4cdabaae0cd1af4a553fa971e0b8177c057d
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-06 09:57:00 +0530

    Finding habitable planets

--- P06_Planet_Project/index.js
+++ P06_Planet_Project/index.js
@@ -5,6 +5,8 @@ const habitablePlanets = [];
 
 function isHabitablePlanet(planet){
     return planet['koi_disposition'] === 'CONFIRMED'
+      && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+      && planet['koi_prad'] < 1.6;
 }
 
 fs.createReadStream('kepler_data.csv')
@@ -20,8 +22,7 @@ fs.createReadStream('kepler_data.csv')
     .on('error', (err) => {
         console.log(err);
     })
-    .on('end',() => {
-        console.log(habitablePlanets); 
-        console.log('done');
+    .on('end',() => { 
+        console.log(`${habitablePlanets.length} habitable planets found!`);
     });
 // parse();

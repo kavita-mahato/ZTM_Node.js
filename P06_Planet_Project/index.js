b0e686db7004dfaa7115aa1dcade75ee4b514dee
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-06 09:41:08 +0530

    Parsing planets data

--- P06_Planet_Project/index.js
+++ P06_Planet_Project/index.js
@@ -1,9 +1,14 @@
-const parse = require('csv-parse');
+const {parse} = require('csv-parse');
 const fs = require('fs');
 
 const results = [];
 
 fs.createReadStream('kepler_data.csv')
+    // pipe is a method that takes a readable stream and turns it into a writable stream
+    .pipe(parse({
+        comment: '#',
+        columns: true, // this will return each row as an javascript object with key value pairs rather than an array of values in a row
+    }))
     .on('data', (data) => {
         results.push(data);
     })

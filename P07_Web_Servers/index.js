69642a9175ff3747b9f2806623e3ece83bfc9ac9
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-06 13:58:19 +0530

    HTTP APIs and Routing

--- P07_Web_Servers/index.js
+++ P07_Web_Servers/index.js
@@ -1,17 +1,37 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 
-        'Content-Type': 'text/plain' 
-    });
+const server = http.createServer();
+
+server.on("request", (req, res) => { 
+  if(req.url === '/friends'){
+    // res.writeHead(200, {
+    //   "Content-Type": "application/json",
+    // });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({
-        id: 1,
-        name: 'Kavita Mahato',
+      id: 1,
+      name: "Kavita Mahato",
     }));
+  }else if(req.url === '/messages'){
+    res.setHeader('Content-Type', 'text/html'); 
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Kavita</li>');
+    res.write('<li>What are your thoughts on Node.js?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+  }else{
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
-server.listen(PORT,() => {
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 }); // 127.0.0.1 => localhost

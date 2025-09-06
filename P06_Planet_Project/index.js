bb8b0ccc284ec8df0ce8a5f8f001125a33ad5be1
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-06 09:52:39 +0530

    Finding habitable planets

--- P06_Planet_Project/index.js
+++ P06_Planet_Project/index.js
@@ -1,22 +1,27 @@
 const {parse} = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+}
 
 fs.createReadStream('kepler_data.csv')
-    // pipe is a method that takes a readable stream and turns it into a writable stream
-    .pipe(parse({
+    .pipe(parse({  // pipe is a method that takes a readable stream and turns it into a writable stream
         comment: '#',
         columns: true, // this will return each row as an javascript object with key value pairs rather than an array of values in a row
     }))
     .on('data', (data) => {
-        results.push(data);
+        if(isHabitablePlanet(data)){
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end',() => {
-        console.log(results); 
+        console.log(habitablePlanets); 
         console.log('done');
     });
 // parse();

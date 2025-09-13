be062cba701d9eb71da30da236f2d41f1851c071
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-13 09:56:48 +0530

    Logging Requests with Morgan

--- P09_NASA_Project/server/src/app.js
+++ P09_NASA_Project/server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');  
+const morgon = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgon('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -16,6 +18,6 @@ app.use(planetsRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
+}); 
 
 module.exports = app;

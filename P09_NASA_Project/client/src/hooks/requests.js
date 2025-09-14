fa3e51b3d5b8176bc135414f0ed54f957ba1da93
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-14 12:56:38 +0530

    DELETE launches Aborting Launches

--- P09_NASA_Project/client/src/hooks/requests.js
+++ P09_NASA_Project/client/src/hooks/requests.js
@@ -40,6 +40,16 @@ async function httpSubmitLaunch(launch) {
 async function httpAbortLaunch(id) {
   // TODO: Once API is ready.
   // Delete launch with given ID.
+  try{
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'DELETE',
+    });
+  }
+  catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 export {
--- P09_NASA_Project/server/src/routes/launches/launches.controller.js
+++ P09_NASA_Project/server/src/routes/launches/launches.controller.js
@@ -1,6 +1,7 @@
 const {
     getAllLaunches,
     addNewLaunch,
+    existsLaunchWithId,
 } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
@@ -22,6 +23,16 @@ function httpAddNewLaunch(req, res) {
     }
     addNewLaunch(launch);
     return res.status(201).json(launch);
+}
+
+function httpAbortLaunch(req, res) {
+    const launchId = Number(req.params.id);
+
+    if (!existsLaunchWithId(launchId)) {
+        return res.status(404).json({
+            error: 'Launch not found',
+        });
+    }
 }
 
 module.exports = {

298d5705ed2b3efbfe627bba09d050ced932f118
Kavita Mahato <kavitamahato0835@gmail.com>
2025-09-30 06:42:12 +0530

    Worker Threads in Action

---
+++ P11_Node_Performance/THREADS/threads.js
@@ -0,0 +1,20 @@
+const {
+    isMainThread, 
+    workerData,
+    Worker,
+} = require('worker_threads');
+
+if (isMainThread) {
+    // This is the main thread
+    console.log(`Main Thread Process ID: ${process.pid}`);
+    new Worker(__filename,{
+        workerData: [7,6,2,3]
+    });
+    new Worker(__filename,{
+        workerData: [1,3,4,3]
+    });
+} else {
+    // This is a worker thread
+    console.log(`Worker Process ID: ${process.pid}`);
+    console.log(`Worker Data: ${workerData} sorted is ${workerData.sort()}`);
+}

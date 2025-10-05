8759bdd68ed613fc0479e9b6763eae8226d06e6b
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-05 16:27:11 +0530

    Setting up our Security example

--- P14_Node_Security/server.js
+++ P14_Node_Security/server.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 const app = express();
 
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret code is 12345');
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });

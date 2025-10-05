873fd5181b7216ee2004282ce6438a0fb81bf9f2
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-05 17:02:25 +0530

    Self Signed Cert. & Public Key Cryptography

--- P14_Node_Security/server.js
+++ P14_Node_Security/server.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
 const express = require('express');
 
 const PORT = 3000;
@@ -13,6 +15,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+// Note: In a real-world scenario, you would need to provide valid SSL certificates for HTTPS.
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
   console.log(`Server is running on ${PORT}...`);
 });
